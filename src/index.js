ab33abdd4c68fd7f55bdbdc7c2b7de3a580835b3
Rehan Willemse <rehan.willemse@gmail.com>
2021-11-20 12:33:00 +0200

    Some changes, error needs fixing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route, Router } from 'react-router-dom'
+import Upload from './containers/Upload'
+import './index.css'
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Router>
+        <Route path='/' component={Home}/>
+        <Route path='/upload' component={Upload}/>
+      </Router>
+    </HashRouter>
+  );
+}
+
+export default App;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
